d675cd3c0fae99e86c735b116a6d22c1ccf76abd
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-24 12:20:35 +0100

    updates to the query method

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function jema () {
 
     if (!this.called) {
         document.getElementById('body').addEventListener('click', function (e) {
-            $base.checkForAction({'type':e.type, 'target':e.target});
+            $base.checkForAction({'type':e.type, 'evt':e});
         });
         this.called = true;
     }
@@ -20,29 +20,31 @@ jema.prototype.fire = function (obj) {
 }
 
 jema.prototype.checkForAction = function (dts) {
-    let {type, target} = dts;
-    let id = target.id;
+    let {type, evt} = dts;
+    let id = evt.target.id;
 
     if (this.items.includes(id) && this.actions[id].type === type) {
-        this.actions[id].action(target);
+        this.actions[id].action(evt);
     }
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
     let lema = new jema();
+    let btn_hd = `<button id="kb">Click me</button> <button id="jd">for jd</button> <button id="dembele">for dembele</button>`;
 
     lema.fire({
         'name':'kb', 'type':'click', 'action': function (event) {
             console.log(event, 'kb is on fire');
+            document.getElementById('root').innerHTML += btn_hd;
         }
     });
 
     lema.fire({
         'name':'jd', 'type':'click', 'action': function (event) {
             console.log(event, 'jd is the man');
+            event.target.style.display = 'none';
         }
     });
 
-    let btn_hd = `<button id="kb">Click me</button> <button id="jd">for jd</button> <button id="dembele">for dembele</button>`;
     document.getElementById('root').innerHTML = btn_hd;
 });
