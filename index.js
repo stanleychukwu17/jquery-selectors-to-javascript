29fc0d28a23eddcf119358f2843faaae4748e5c8
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-24 10:52:02 +0100

    updates to the query method

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-function jema (element) {
-    this.element = element;
-    console.log(element);
+function jema (obj) {
+    this.called = false;
+    this.items = [];
+    this.actions = {};
+
+    if (!this.called) {
+        console.log('now add event listner to the body tag');
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    jema('kb');
-    let btn_hd = `<button id="kb">Click me</button>`;
+    jema({
+        'name':'kb', 'event':'click', 'action': function () {
+            console.log('clicked');
+        }
+    });
+
+    jema({
+        'name':'jd', 'event':'click', 'action': function () {
+            console.log('clicked');
+        }
+    });
+
+    let btn_hd = `<button id="kb">Click me</button> <button id="jd">for jd</button>`;
     document.getElementById('root').innerHTML = btn_hd;
 });
